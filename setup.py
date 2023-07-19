5807b296f452ff10e042b40ca9ceffb1e31a501b
OneFallDen <onefallden@gmail.com>
2023-07-19 23:33:11 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="FastApiLikePosts",
+    version="1.0.0",
+    url="https://github.com/OneFallDen/FastApiLikePosts",
+    packages=find_packages()
+)
