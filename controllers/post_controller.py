0fdb6986713f490decf156936fe94bf35fdea0da
OneFallDen <onefallden@gmail.com>
2023-07-20 00:48:17 +0300

    added main requests

---
+++ controllers/post_controller.py
@@ -0,0 +1,37 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException
+
+from models import schemas
+from db.crud import get_post, add_post, update_post, delete_post
+
+
+def post_get(postId: int, db: Session):
+    try:
+        return get_post(postId, db)
+    except:
+        raise HTTPException(status_code=404, detail='Post not found')
+
+
+def post_add(post: schemas.AddOrUpdatePost, accountId: int, db: Session):
+    return add_post(post, accountId, db)
+
+
+def post_update(postId: int, post: schemas.AddOrUpdatePost, accountId: int, db: Session):
+    try:
+        post = get_post(postId, db)
+        if post['owner'] != accountId:
+            raise HTTPException(status_code=403)
+    except:
+        raise HTTPException(status_code=404, detail='Post not found')
+    update_post(postId, post, db)
+    return get_post(postId, db)
+
+
+def post_delete(postId: int, accountId: int, db: Session):
+    try:
+        post = get_post(postId, db)
+        if post['owner'] != accountId:
+            raise HTTPException(status_code=403)
+    except:
+        raise HTTPException(status_code=404, detail='Post not found')
+    delete_post(postId, db)
--- db/crud.py
+++ db/crud.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 from fastapi import HTTPException
 
 from db import models
+from models import schemas
 
 
 """
@@ -22,12 +23,7 @@ def signup_user(firstname: str, lastname: str, email: str, password: str, db: Se
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
-    return {
-        'id': db_user.id,
-        'firstname': firstname,
-        'lastname': lastname,
-        'email': email
-    }
+    return db_user.id
 
 
 def check_email(email: str, db: Session):
@@ -38,3 +34,40 @@ def check_email(email: str, db: Session):
 def get_user(username: str, db: Session):
     result = db.execute(select(models.Account).where(models.Account.email == username)).first()
     return result[0]
+
+
+"""
+    POST
+"""
+
+
+def get_post(postId: int, db: Session):
+    result = db.execute(select(models.Post).where(models.Post.id == postId)).first()
+    return result[0]
+
+
+def add_post(post: schemas.AddOrUpdatePost, accountId: int, db: Session):
+    db_post = models.Post(
+        title=post.title,
+        content=post.content,
+        owner=accountId,
+    )
+    db.add(db_post)
+    db.commit()
+    db.refresh(db_post)
+    return db_post
+
+
+def update_post(postId, post: schemas.AddOrUpdatePost, db: Session):
+    db.query(models.Post).filter(models.Post.id == postId).update(
+        {
+            models.Post.title: post.title,
+            models.Post.content: post.content
+        }
+    )
+    db.commit()
+
+
+def delete_post(postId: int, db: Session):
+    db.query(models.Post).filter(models.Post.id == postId).delete()
+    db.commit()
