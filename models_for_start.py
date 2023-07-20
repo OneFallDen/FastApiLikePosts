574280928791c491e031d03f4016cbd061ad8716
OneFallDen <onefallden@gmail.com>
2023-07-20 18:15:14 +0300

    docker fix

---
+++ models_for_start.py
@@ -0,0 +1,47 @@
+import sys
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+from db.database import engine
+
+Base = declarative_base()
+
+
+class Account(Base):
+    __tablename__ = 'account'
+
+    id = Column(Integer, primary_key=True, index=True)
+    firstname = Column(String(255), nullable=False)
+    lastname = Column(String(255), nullable=False)
+    password = Column(String(255), nullable=False)
+    email = Column(String(255), nullable=False)
+
+    owner_post = relationship('Post', backref='account')
+
+
+class Post(Base):
+    __tablename__ = 'post'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(255), nullable=False)
+    content = Column(String(511), nullable=False)
+    owner = Column(Integer, ForeignKey('account.id'), nullable=False)
+    likes = Column(Integer, nullable=False, default=0)
+    dislikes = Column(Integer, nullable=False, default=0)
+
+
+def create_db_and_tables():
+    Base.metadata.create_all(engine)
+
+
+def drop_tables():
+    Base.metadata.drop_all(engine)
+
+
+if __name__ == '__main__':
+    if sys.argv[1] == 'createdb':
+        create_db_and_tables()
+    elif sys.argv[1] == 'dropdb':
+        drop_tables()
